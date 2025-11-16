04ae22cace9262bd8df58d9b44a7432dd743fb56
lazamal <lazamal@gmail.com>
2025-11-16 16:36:12 +0200

    mid building animation functions

--- my_types.py
+++ my_types.py
@@ -1,9 +1,39 @@
+from enum import Enum, auto
+from utils import lerp
+
+class HorizontalState(Enum):
+    IDLE = auto()
+    MOVING_LEFT = auto()
+    MOVING_RIGHT = auto()
+
 class Animation():
     def __init__(self, start, end, t, state, direction, duration, final):
-        self.start_point_of_animation = 0
-        self.end_point_of_animation = 0
-        self.t_of_animation = 0
-        self.state_of_animation = 0
-        self.direction_of_animation = 0
-        self.duration_of_animation = 0
-        self.final_animation_value = 0
+        self.start = start
+        self.end = end
+        self.t = t
+        self.state = state
+        self.direction = direction
+        self.duration = duration   
+        self.final = final
+
+    def start_movement(self, new_start, target, direction, duration):
+        if self.state == HorizontalState.IDLE:
+            self.state = (
+            HorizontalState.MOVING_RIGHT if direction > 0 else HorizontalState.MOVING_LEFT
+        )
+            self.start = new_start
+            self.ending_movement = self.start + target * direction
+            self.movement_t = 0
+            self.movement_duration = duration
+
+    def update_movement(self,dt):
+        if self.state != HorizontalState.IDLE:
+            self.t += dt / self.duration
+            if self.t >= 1:
+                self.t=1
+                self.final = self.ending_movement
+                self.state = HorizontalState.IDLE
+            else:
+                
+                self.final = lerp(self.starting_movement, self.ending_movement, self.movement_t)
+                return self.final
